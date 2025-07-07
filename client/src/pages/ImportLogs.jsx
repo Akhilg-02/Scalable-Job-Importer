99bb90b835b82aa7a76adac519338cca3955218b
unknown <guptaakhil0206@gmail.com>
2025-07-07 12:16:10 +0530

    Enhanced the UI

--- client/src/pages/ImportLogs.jsx
+++ client/src/pages/ImportLogs.jsx
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from 'react'
 import axios from 'axios';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow } from "@mui/material";
+import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TablePagination, TableRow, CircularProgress } from "@mui/material";
 
 const apiUrl = import.meta.env.VITE_API_URL  // || "http://localhost:8000" 
 
@@ -34,6 +34,7 @@ const ImportLogs = () => {
     const [logs, setLogs] = useState([]);
     const [page, setPage] = useState(0);
     const [rowsPerPage, setRowsPerPage] = useState(10);
+    const [progress, setProgress] = useState(0);
 
     useEffect(() => {
         const fetchImportLogs = async () => {
@@ -49,60 +50,88 @@ const ImportLogs = () => {
         fetchImportLogs();
     }, []);
 
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10));
+        }, 800);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, [])
+
     const handleChangePage = (event, newPage) => setPage(newPage);
     const handleChangeRowsPerPage = (event) => {
         setRowsPerPage(+event.target.value);
         setPage(0);
     };
-
-    if (loading) return <p style={{ padding: "2rem" }}>Loading...</p>;
     return (
         <>
-            <Paper sx={{ width: "100%", overflow: "hidden", p: 2 }}>
-                <h2>📦 Import Logs</h2>
-                <TableContainer sx={{ maxHeight: 500 }}>
-                    <Table stickyHeader>
-                        <TableHead>
-                            <TableRow>
-                                {columns.map((col) => (
-                                    <TableCell
-                                        key={col.id}
-                                        style={{ minWidth: col.minWidth }}
-                                        align={col.align || "left"}
-                                        sx={{backgroundColor:"#E8E8E8"}}
-                                    >
-                                        {col.label}
-                                    </TableCell>
-                                ))}
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {logs
-                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                .map((log) => (
-                                    <TableRow key={log._id} hover>
-                                        {columns.map((col) => {
-                                            const value = log[col.id];
-                                            return (
-                                                <TableCell key={col.id} align={col.align || "left"}>
-                                                    {col.format ? col.format(value) : value}
-                                                </TableCell>
-                                            );
-                                        })}
+        <h1>📦 Import Logs</h1>
+            <Paper sx={{ width: "99%", overflow: "hidden", p: 2 }}>
+                
+
+                {loading && (
+                    <Box display="flex" justifyContent="center" alignItems="center" height="300px">
+                        <CircularProgress variant="determinate" value={progress} />
+                    </Box>
+                )}
+                {!loading && logs.length === 0 && (
+                    <Box display="flex" justifyContent="center" alignItems="center" height="300px">
+                        <Typography variant="h6" color="text.secondary">
+                            No logs found.
+                        </Typography>
+                    </Box>
+                )}
+
+                {!loading && logs.length > 0 && (
+                    <>
+                        <TableContainer sx={{ maxHeight: 500 }}>
+                            <Table stickyHeader>
+                                <TableHead>
+                                    <TableRow>
+                                        {columns.map((col) => (
+                                            <TableCell
+                                                key={col.id}
+                                                style={{ minWidth: col.minWidth }}
+                                                align={col.align || "left"}
+                                                sx={{ backgroundColor: "#E8E8E8" }}
+                                            >
+                                                {col.label}
+                                            </TableCell>
+                                        ))}
                                     </TableRow>
-                                ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    component="div"
-                    count={logs.length}
-                    page={page}
-                    rowsPerPage={rowsPerPage}
-                    rowsPerPageOptions={[5, 10, 25]}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
+                                </TableHead>
+                                <TableBody>
+                                    {logs
+                                        .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                        .map((log) => (
+                                            <TableRow key={log._id} hover>
+                                                {columns.map((col) => {
+                                                    const value = log[col.id];
+                                                    return (
+                                                        <TableCell key={col.id} align={col.align || "left"}>
+                                                            {col.format ? col.format(value) : value}
+                                                        </TableCell>
+                                                    );
+                                                })}
+                                            </TableRow>
+                                        ))}
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                        <TablePagination
+                            component="div"
+                            count={logs.length}
+                            page={page}
+                            rowsPerPage={rowsPerPage}
+                            rowsPerPageOptions={[5, 10, 25]}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                        />
+
+                    </>
+                )}
             </Paper>
 
         </>
